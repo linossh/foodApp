8fa04b2b70f1bea35f0dc68cc8f4becb6bc22a67
joaolino <joaolslinoo@gmail.com>
2024-12-19 17:55:49 +0000

    finished Restaurant.kt

--- app/src/main/java/com/example/foodapp/Restaurant.kt
+++ app/src/main/java/com/example/foodapp/Restaurant.kt
@@ -9,13 +9,16 @@ data class Restaurant(
 
 // Dados fictícios para os restaurantes
 val restaurantList = listOf(
-    Restaurant("Pizza Palace", R.drawable.pizza_restaurant, 95, "15-20 min"),
-
-    Restaurant("Burger Spot", R.drawable.burger_restaurant, 90, "10-15 min")
-    /**
-    Restaurant("Sushi Express", R.drawable.sushi, 92, "20-25 min"),
-    Restaurant("Pasta House", R.drawable.pasta, 88, "25-30 min"),
-    Restaurant("Taco Town", R.drawable.taco, 85, "15-20 min"),
-     **/
-
+        Restaurant("Burger Spot", R.drawable.burger_restaurant, 90, "10-15 min"),
+        Restaurant("Pizza Palace", R.drawable.pizza_restaurant, 95, "15-20 min"),
+        Restaurant("Sushi Express", R.drawable.sushi_restaurant, 92, "20-25 min"),
+        Restaurant("Pasta House", R.drawable.pasta_restaurant, 88, "25-30 min"),
+        Restaurant("Taco Town", R.drawable.taco_restaurant, 85, "15-20 min"),
+        Restaurant("kebab King", R.drawable.kebab_restaurant, 85, "15-20 min"),
+        Restaurant("Burger Spot", R.drawable.burger_restaurant, 90, "10-15 min"),
+        Restaurant("Pizza Palace", R.drawable.pizza_restaurant, 95, "15-20 min"),
+        Restaurant("Sushi Express", R.drawable.sushi_restaurant, 92, "20-25 min"),
+        Restaurant("Pasta House", R.drawable.pasta_restaurant, 88, "25-30 min"),
+        Restaurant("Taco Town", R.drawable.taco_restaurant, 85, "15-20 min"),
+        Restaurant("kebab King", R.drawable.kebab_restaurant, 85, "15-20 min")
 )
